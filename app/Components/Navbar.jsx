12b76895dee5db6e69484d27ddc8dc5d1bbf92b7
Coder030 <garg2012030@gmail.com>
2025-01-16 23:10:25 +0530

    responsive

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -13,30 +13,25 @@ export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const links = [
-    { target: "/", label: "/page1", active: pathname === "/" },
-    { target: "/page2", label: "/page2", active: pathname === "/page2" },
-    { target: "/page3", label: "/page3", active: pathname === "/page3" },
-    {
-      target: "/page4",
-      label: "/page4",
-      active: pathname === "/page4",
-    }
+    { target: "/", label: "Page 1", active: pathname === "/" },
+    { target: "/page2", label: "Page 2", active: pathname === "/page2" },
+    { target: "/page3", label: "Page 3", active: pathname === "/page3" },
+    { target: "/page4", label: "Page 4", active: pathname === "/page4" },
   ];
 
   return (
-    <div className="flex items-center gap-[0px] w-full p-[30px] pl-[100px] border-b">
-
+    <div className="flex items-center gap-[0px] w-full p-[30px] pl-[50px] border-b">
       {/* Logo */}
       <Link href="/">
         <img
           src="/logo.ico"
           alt="Logo"
-          className="h-[40px] relative bottom-[5px] sm:mr-[10px] sm:h-[40px] md:h-[50px]"
+          className="h-[40px] relative bottom-[5px] sm:mr-[20px] sm:h-[40px] md:h-[50px]"
         />
       </Link>
 
       {/* Desktop Navbar Links */}
-      <div className="flex items-center gap-[30px] flex-1 ml-[100px]">
+      <div className="hidden sm:flex items-center gap-[30px] flex-1 ml-[5%]">
         {links.map((link, i) => (
           <a
             key={i}
@@ -73,8 +68,8 @@ export default function Navbar() {
         ))}
       </div>
 
-      {/* Buttons */}
-      <div className="flex gap-[10px] ml-auto mr-[100px]">
+      {/* Desktop Buttons */}
+      <div className="hidden sm:flex gap-[10px] ml-auto">
         <button className="bg-blue-500 font-semibold text-[18px] cursor-pointer transition-all duration-200 px-4 py-2 rounded text-[#fff] hover:bg-blue-600 hover:shadow-lg">
           Sign Up
         </button>
@@ -95,59 +90,55 @@ export default function Navbar() {
 
       {/* Mobile Menu */}
       {menuOpen && (
-        <div>
-          <div
-            className={`fixed flex flex-col justify-center items-center z-10 top-0 right-0 h-full w-[50%] bg-white text-black transition-transform duration-300 transform ${
-              menuOpen ? "translate-x-0" : "translate-x-full"
-            }`}
-          >
-            <button
-              className="absolute right-7 top-4 p-3"
-              onClick={() => setMenuOpen(!menuOpen)}
-            >
-              X{" "}
-            </button>
-            {links.map((link, i) => (
-              <a
-                key={i}
-                href={link.target}
-                onMouseEnter={() => {
-                  setHoverIndicator(true);
-                  setHoverIndicatorPosition(i);
-                }}
-                onMouseLeave={() => {
-                  setHoverIndicator(false);
-                  setHoverIndicatorPosition(0);
-                }}
-              >
-                <div
-                  className={`font-semibold text-[25px] cursor-pointer transition-all duration-200 ${
-                    link.active ||
-                    (hoverIndicator && hoverIndicatorPosition === i)
-                      ? "text-[#4a4a4a]" // Active color
-                      : "text-[#9e9e9e]"
-                  }`}
-                >
-                  {link.label}
-                </div>
+        <div className="fixed top-0 right-0 bg-white z-10 flex flex-col items-center justify-center w-[50%] h-full">
 
-                <div
-                  className={`transform transition-all duration-400 h-[8px] relative top-[-6px] w-[calc(100%+10px)] ml-[-5px] origin-bottom ${
-                    (hoverIndicator && hoverIndicatorPosition === i) ||
-                    (!hoverIndicator && link.active)
-                      ? "bg-[#008AE3] opacity-[20%] top-[-15px] transform scale-y-100"
-                      : "bg-transparent transform scale-y-0"
-                  }`}
-                />
-              </a>
-            ))}
-            <button className="bg-blue-500 text-white px-4 py-2 rounded mb-[10px] hover:bg-blue-600 hover:shadow-lg">
-              Sign Up
-            </button>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded mb-[10px] hover:bg-blue-600 hover:shadow-lg">
-              Sign In
-            </button>
-          </div>
+          <button
+            className="absolute top-4 right-4 text-2xl"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            X
+          </button>
+          {links.map((link, i) => (
+            <a
+              key={i}
+              href={link.target}
+              className="my-2 text-xl"
+              onMouseEnter={() => {
+                setHoverIndicator(true);
+                setHoverIndicatorPosition(i);
+              }}
+              onMouseLeave={() => {
+                setHoverIndicator(false);
+                setHoverIndicatorPosition(0);
+              }}
+            >
+              <div
+                className={`font-semibold text-[25px] cursor-pointer transition-all duration-200 ${
+                  link.active ||
+                  (hoverIndicator && hoverIndicatorPosition === i)
+                    ? "text-[#4a4a4a]" // Active color
+                    : "text-[#9e9e9e]"
+                }`}
+              >
+                {link.label}
+              </div>
+
+              <div
+                className={`transform transition-all duration-400 h-[8px] relative top-[-6px] w-[calc(100%+10px)] ml-[-5px] origin-bottom ${
+                  (hoverIndicator && hoverIndicatorPosition === i) ||
+                  (!hoverIndicator && link.active)
+                    ? "bg-[#008AE3] opacity-[20%] top-[-15px] transform scale-y-100"
+                    : "bg-transparent transform scale-y-0"
+                }`}
+              />
+            </a>
+          ))}
+          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-[10px] hover:bg-blue-600 hover:shadow-lg">
+            Sign Up
+          </button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-[10px] hover:bg-blue-600 hover:shadow-lg">
+            Sign In
+          </button>
         </div>
       )}
     </div>
